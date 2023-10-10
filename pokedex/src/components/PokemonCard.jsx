3e023febd9b7e013c2f760d51d408401a14a879f
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-10-10 12:00:20 +0200

    challenge React Basics 02

---
+++ pokedex/src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function Pokemon() {
+    return  <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon Bulbasaur" />
+                <figcaption>Bulbasaur</figcaption>
+            </figure>
+}
+
+export default Pokemon;
